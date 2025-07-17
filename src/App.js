55a473ce315d2d2dcaeda3f598904678d31d65f2
Ive21Go <nadia.ivette.gz@gmail.com>
2025-07-17 17:44:21 -0600

    Add dynamic contact list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import { Contact } from './components';
-
+import { useState, useEffect } from 'react';
 
 function App() {
+  // const [State, Function-State] = useState([]);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    // const saveContacts = localStorage.getItem('contacts');
+    // if (saveContacts) {
+    //   setContacts(JSON.parse(saveContacts));
+    // }
+    setContacts([
+    { id:'01', name: 'John Doe', title: 'Software Engineer', email: 'jone@done.com', phone: '123-456-7890', avatar: 'male' },
+    { id:'02', name: 'Doe Juan', title: 'Software Engineer', email: 'jone2@done.com', phone: '123-456-90', avatar: 'female' },
+    { id:'03', name: 'Marie', title: 'Software Engineer', email: 'marie@done.com', phone: '123-6-7890', avatar: 'female' },
+    { id:'04', name: 'Pedrito', title: 'Software Engineer', email: 'ejemplo@done.com', phone: '3-456-7890', avatar: 'account' }
+    ])
+  }, []);
+
   return (
     <div className="App">
       <div className="container my-5">
@@ -13,31 +29,17 @@ function App() {
           </div>
         </div>
         <div className="row g-4 justify-content-center">
-          <div className="col-12 col-md-4 d-flex justify-content-center">
-            <Contact
-              name="John Doe"
-              title="Software Engineer"
-              email="jone@done.com"
-              phone="123-456-7890"
-              avatar="female" />
-          </div>
-          <div className="col-12 col-md-4 d-flex justify-content-center">
-            <Contact
-              name="John Doe"
-              title="Software Engineer"
-              email="jone@done.com"
-              phone="123-456-7890"
-              avatar="female" />
-          </div>
-          <div className="col-12 col-md-4 d-flex justify-content-center">
-            <Contact
-              name="John Doe"
-              title="Software Engineer"
-              email="jone@done.com"
-              phone="123-456-7890"
-              avatar="female"
-            />
-          </div>
+          {contacts.map((contact) => (
+            <div key={contact.id} className="col-12 col-md-4 d-flex justify-content-center">
+              <Contact
+                name={contact.name}
+                title={contact.title}
+                email={contact.email}
+                phone={contact.phone}
+                avatar={contact.avatar}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
