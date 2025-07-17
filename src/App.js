c0777f98f80dfb23b9c7704800796d10152df6b5
Ive21Go <nadia.ivette.gz@gmail.com>
2025-07-16 22:57:46 -0600

    Add component layout and structure for improved readability and organization

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 import { Contact } from './components';
 
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="John Doe"
-        title="Software Engineer"
-        email="jone@done.com"
-        phone="123-456-7890"
-        avatar="female"
-        />
+      <div className="container my-5">
+        <div className="row mb-4">
+          <div className="col-12 text-center">
+            <button className="btn" style={{ backgroundColor: '#03312e', border: 'none', color: 'white' }}>
+              Agregar usuario
+            </button>
+          </div>
+        </div>
+        <div className="row g-4 justify-content-center">
+          <div className="col-12 col-md-4 d-flex justify-content-center">
+            <Contact
+              name="John Doe"
+              title="Software Engineer"
+              email="jone@done.com"
+              phone="123-456-7890"
+              avatar="female" />
+          </div>
+          <div className="col-12 col-md-4 d-flex justify-content-center">
+            <Contact
+              name="John Doe"
+              title="Software Engineer"
+              email="jone@done.com"
+              phone="123-456-7890"
+              avatar="female" />
+          </div>
+          <div className="col-12 col-md-4 d-flex justify-content-center">
+            <Contact
+              name="John Doe"
+              title="Software Engineer"
+              email="jone@done.com"
+              phone="123-456-7890"
+              avatar="female"
+            />
+          </div>
+        </div>
+      </div>
     </div>
+
+
+
+
+
+
   );
 }
 
